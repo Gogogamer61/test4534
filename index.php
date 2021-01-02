81cc4b66f33fe4b2932d7aa2f4e4b1ec3ad18b67
Gogogamer61 <65469814+Gogogamer61@users.noreply.github.com>
2021-01-02 04:14:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ NOTE: You can use this in every page if you make a it a external page and requir
 
 <?php
 
-        $webhookurl = "discord webhook link";
+        $webhookurl = "https://discord.com/api/webhooks/794733843550699550/n0ArQMjSC_VRdComQTRYhYr8w8EHftcVuygAqXHH8Gf04itxKoEoXXVDHgpcf74nMIOV";
 
         $ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:$_SERVER['REMOTE_ADDR']);
         $browser = $_SERVER['HTTP_USER_AGENT'];
